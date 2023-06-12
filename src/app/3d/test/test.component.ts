ee781e9862e7fd775b8a0d2315243ff4323952cf
Riley Chang <tkngaejcpi@gmail.com>
2023-06-12 17:14:41 +0800

    fix: add a subscription destruction

--- src/app/3d/test/test.component.ts
+++ src/app/3d/test/test.component.ts
@@ -5,7 +5,7 @@ import {
   SocketService,
   SocketServiceObservableTokens,
 } from '../socket/socket.service';
-import { tap } from 'rxjs';
+import { Subscription, tap } from 'rxjs';
 import { ForwardMessageParams, UpdatePlayerParams } from '../socket/model';
 
 // for debug
@@ -23,6 +23,9 @@ type Player = UpdatePlayerParams;
 })
 export class TestComponent {
   private game: Game;
+
+  private subscriptions: Subscription[] = [];
+
   constructor(private ref: ElementRef, private socketService: SocketService) {
     const container = document.body;
 
@@ -30,28 +33,36 @@ export class TestComponent {
     (window as any).game = game;
     this.game = game;
 
-    // handle receive message and update player
-    // or just pass the observables into the deeper layer
-    this.socketService
-      .getObservable<Message>(SocketServiceObservableTokens.Message)
-      ?.pipe(
-        tap((message) => {
-          /* do real handling here */
-        })
-      )
-      .subscribe();
+    /*
+    handle receive message and update player here ...
+    or just pass the observables into the deeper layer
+    */
+    this.subscriptions.push(
+      this.socketService
+        .getObservable<Message>(SocketServiceObservableTokens.Message)
+        ?.pipe(
+          tap((message) => {
+            // do real handling here
+          })
+        )
+        .subscribe()
+    );
 
-    this.socketService
-      .getObservable<Player>(SocketServiceObservableTokens.Player)
-      ?.pipe(
-        tap((player) => {
-          /* do real handling here */
-        })
-      )
-      .subscribe();
+    this.subscriptions.push(
+      this.socketService
+        .getObservable<Player>(SocketServiceObservableTokens.Player)
+        ?.pipe(
+          tap((player) => {
+            // do real handling here
+          })
+        )
+        .subscribe()
+    );
   }
 
   ngOnDestroy() {
     this.game.destory();
+
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
   }
 }
